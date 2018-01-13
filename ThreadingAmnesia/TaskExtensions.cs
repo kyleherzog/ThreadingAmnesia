b1ad0aab62a6abad9c616db52c72af10c3d3a5c1
Kyle Herzog <krherzog@bluebunny.com>
2018-01-13 15:45:53 -0600

    Initial Commit

---
+++ ThreadingAmnesia/TaskExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ThreadingAmnesia
+{
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task)
+        {
+            // Consider it forgotten 
+        }
+    }
+}
